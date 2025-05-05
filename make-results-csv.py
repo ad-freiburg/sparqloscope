d79aa426332f2f66e5c701319cc50d2d3b85ce3b
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-05-05 09:38:59 +0200

    Program to generate csv table with running times

---
+++ make-results-csv.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+
+# Copyright 2025, University of Freiburg,
+# Chair of Algorithms and Data Structures
+# Author: Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+
+import argparse
+import argcomplete
+import re
+import yaml
+from typing import Optional
+from pathlib import Path
+import csv
+
+RESULTS_YAML_FILENAME_RE = re.compile(
+    r'^(?P<dataset>[a-zA-Z]+)\.(?P<engine>[a-zA-Z]+)\.results\.ya?ml$')
+BAD_COL_NAME_RE = re.compile(r'[^a-zA-Z]+')
+
+# Type for a parsed result: float represents seconds, None represents error
+SingleDatasetAndEngineResultsDict = dict[str, Optional[float]]
+# Dataset name, engine name and results dict
+SingleDatasetAndEngineResults = tuple[str, str,
+                                      SingleDatasetAndEngineResultsDict]
+
+
+def load_results_yaml(filename: str) \
+        -> SingleDatasetAndEngineResults:
+    """
+    Load a results yaml file and extract the required values.
+    """
+    fn_match = RESULTS_YAML_FILENAME_RE.match(Path(filename).name)
+    assert fn_match, \
+        "The filename should be of the form DATASET.ENGINE.results.yaml " + \
+        "and match the regular expression: " + RESULTS_YAML_FILENAME_RE.pattern
+    with open(filename, "r") as yaml_file:
+        res = yaml.safe_load(yaml_file)
+    dataset = fn_match.group("dataset")
+    engine = fn_match.group("engine")
+    query_results: SingleDatasetAndEngineResultsDict = {}
+    for query_result in res["queries"]:
+        name = query_result["query"].split()[0]
+        if query_result["headers"] == [] and \
+                isinstance(query_result["results"], str):
+            # Error
+            qtime = None
+        else:
+            qtime = query_result["runtime_info"]["client_time"]
+        query_results[name] = qtime
+    return dataset, engine, query_results
+
+
+# Dict maps query names to results for each (dataset, engine)
+AllResultsDict = dict[str, dict[tuple[str, str], Optional[float]]]
+
+
+def merge_multiple_results(parsed: list[SingleDatasetAndEngineResults]) \
+        -> AllResultsDict:
+    """
+    Merge and invert SingleDatasetAndEngineResults for output
+    """
+    by_query: AllResultsDict = {}
+    for dataset, engine, query_results in parsed:
+        for query_name, query_time in query_results.items():
+            if query_name not in by_query:
+                by_query[query_name] = {}
+            by_query[query_name][(dataset, engine)] = query_time
+    return by_query
+
+
+def filter_results_dict(inp: AllResultsDict, filters: list[str]):
+    """
+    Filters the AllResultsDict in-place by applying the regex filters on
+    each query name.
+    """
+
+    if not filters:
+        return
+
+    # Compile regexes
+    rexps: list[re.Pattern] = []
+    for re_str in filters:
+        rexps.append(re.compile(re_str))
+
+    # Apply filters
+    for query_name in list(inp.keys()):
+        if not any(r.search(query_name) for r in rexps):
+            del inp[query_name]
+
+
+def make_column_name(dataset: str, engine: str) -> str:
+    """
+    Make a safe column name that is suitable for LaTeX csvreader (that is,
+    contains only letters a-z and A-Z)
+    """
+    clean_d = BAD_COL_NAME_RE.sub("x", dataset)
+    clean_e = BAD_COL_NAME_RE.sub("x", engine)
+    return clean_d.lower() + clean_e.capitalize()
+
+
+def make_csv_head(inp: AllResultsDict) -> list[str]:
+    """
+    Generate the CSV head for csv.DictWriter
+    """
+    res = ["queryname"]
+    for dataset, engine in next(iter(inp.values())):
+        res.append(make_column_name(dataset, engine))
+    assert len(res) == len(set(res)), \
+        "Multiple columns in the output would be mapped to the same " + \
+        "name due to bad characters in the input filenames"
+    return res
+
+
+def write_csv(inp: AllResultsDict, filename: str):
+    """
+    Write the results to a csv file with the given name.
+    """
+    with open(filename, "w") as f:
+        writer = csv.DictWriter(f, make_csv_head(inp), quoting=csv.QUOTE_NONE)
+        writer.writeheader()
+        for query_name, query_results in inp.items():
+            row = {"queryname": query_name}
+            for (dataset, engine), qtime in query_results.items():
+                val = f"{qtime:.2f}" if qtime else "Error"
+                row[make_column_name(dataset, engine)] = val
+            writer.writerow(row)
+
+
+def command_line_args() -> argparse.Namespace:
+    """
+    Parse the command line arguments and return them
+    """
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "yaml_files", nargs='+', type=str,
+        help="""The result yaml files produced by the 'qlever' command-line
+        script during execution of a benchmark. Their filenames must be of the
+        form DATASET.ENGINE.results.yaml.""")
+    arg_parser.add_argument(
+        "--filter-queries", nargs='*', type=str,
+        help="""
+        Regular expressions to match query names. If any of the filters matches,
+        the results for the respective query are included from all input files.
+        """
+    )
+    arg_parser.add_argument(
+        "--output", nargs=1, type=str, required=True,
+        help="The filename for the output CSV file."
+    )
+
+    argcomplete.autocomplete(arg_parser, always_complete_options="long")
+    args = arg_parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    """
+    Main procedure
+    """
+    # Parse the command line arguments.
+    args = command_line_args()
+
+    # Read input
+    parsed: list[SingleDatasetAndEngineResults] = []
+    for fn in args.yaml_files:
+        parsed.append(load_results_yaml(fn))
+
+    # Merge input
+    merged = merge_multiple_results(parsed)
+    filter_results_dict(merged, args.filter_queries or [])
+
+    # Make csv output file
+    write_csv(merged, args.output[0])
