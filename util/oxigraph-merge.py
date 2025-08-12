2ad99c61ce345fdc408bef07a9db1a5d51f958fa
C. Ullinger <ullingec@informatik.uni-freiburg.de>
2025-08-12 10:34:25 +0200

    Add  benchmark helper script and results for Oxigraph on DBLP (#9)

---
+++ util/oxigraph-merge.py
@@ -0,0 +1,12 @@
+# Script to merge the individual result yamls from `oxigraph-helper.sh`.
+import yaml
+y = {
+    "timeout": 180,
+    "queries": []
+}
+for i in range(1, 106):
+    with open(f"dblp.oxigraph-{i}.results.yaml", "r") as yaml_file:
+        x = yaml.safe_load(yaml_file)
+        y["queries"].extend(x["queries"])
+
+print(yaml.dump(y, sort_keys=False))
